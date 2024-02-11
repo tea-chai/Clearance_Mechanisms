5d9046db2186c013524e6fa874ff50928dcba5d9
kamil erdayandi <12394141+tea-chai@users.noreply.github.com>
2024-02-11 14:34:10 +0300

    Update PFET.py

--- PFET.py
+++ PFET.py
@@ -38,10 +38,10 @@ Lambda= 40.1;
 STOP_difference = 0.05;
 
 
-#DATES = "_21_April"; Total_TIME = 24;
+DATES = "_21_April"; Total_TIME = 24;
 #DATES = "_6_November"; Total_TIME = 24;
 #DATES = "_August"; Total_TIME = 744;
-DATES = "_January"; Total_TIME = 744;
+#DATES = "_January"; Total_TIME = 744;
 #DATES = ""; Total_TIME = 8784;
 
 EXCEEDED = 0; 
